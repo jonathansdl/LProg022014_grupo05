db1f63a9bf4d497c25d69c50bc60cf87d2974ea1
iesb <iesb@iesb-ThinkCentre-M58e.(none)>
2014-08-18 21:52:41 -0300

    modelo de cadastro

---
+++ cadastro/src/modelo/usuario.java
@@ -0,0 +1,49 @@
+package modelo;
+
+public class usuario {
+    Long id;
+    String nome;
+    String cpf;
+    String email;
+    String telefone;
+
+    public String getCPF() {
+        return cpf;
+    }
+
+    public void setCPF(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTel() {
+        return telefone;
+    }
+
+    public void setTel(String telefone) {
+        this.telefone = telefone;
+    }
+}
